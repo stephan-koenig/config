64562b2f06949823dd2aef8c2bd83fd2432406f5
Stephan Koenig <stephan.koenig@gmail.com>
2022-05-30 11:55:54 -0700

    Include instructions for creating bookmarks

--- .config/zsh/bookmarks.zsh
+++ .config/zsh/bookmarks.zsh
@@ -1,3 +1,5 @@
+# To create bookmark, `ln -s ~/.bookmarks/@<shortcut> ~/path/to/<shortcut>`
+
 # Bookmarks for shell navigation
 if [ -d "$HOME/.bookmarks" ]; then
   export CDPATH=".:$HOME/.bookmarks:/"
