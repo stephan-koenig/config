5d037a35d082ac5595475e6fd19a8cdf1ffc209f
Stephan Koenig <stephan.koenig@gmail.com>
2024-03-21 11:26:59 -0700

    Upgrade GitHub and Quarto extensions and Nix

--- .config/zsh/updateall.zsh
+++ .config/zsh/updateall.zsh
@@ -1,8 +1,14 @@
 updateall () {
 	brew upgrade
-	nix flake update --flake $HOME/.config/home-manager && home-manager switch
+	# sudo -i nix upgrade-nix
+	nix flake update --flake $HOME/.config/home-manager \
+          && home-manager switch
 	mas upgrade
 	antidote update --bundles
+	# Update extensions
+	# op plugin run -- gh extension upgrade --all gh extension upgrade --all
+	# quarto update tool chromium
+	# quarto update tool tinytex
 	# Rscript -e 'update.packages()'
 	# conda update conda
 }
