c969faaa23b2bb212a1796938c262d7b605ff58a
Stephan Koenig <stephan.koenig@gmail.com>
2024-09-19 11:41:33 -0700

    Update fonts

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -19,13 +19,13 @@ config.colors = {
 	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
 }
 
-config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
 config.font_size = 19
 
 config.enable_tab_bar = false
 
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
