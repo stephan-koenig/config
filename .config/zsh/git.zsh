f19f9f3b4334d162b3c8a82f8d098f07589e3e13
Stephan Koenig <stephan.koenig@gmail.com>
2025-04-04 16:45:21 -0700

    Move alias

--- .config/zsh/git.zsh
+++ .config/zsh/git.zsh
@@ -1,10 +1,3 @@
-# Overwite git plugin alias
-alias glg="
-  git log --all --graph \
-    --format='format:%w(0,2,6)%C(bold blue)%h - \
-    %C(bold cyan)%aD %C(bold yellow)(%ar)%C(auto)%d%n\
-    %s%C(dim white) - %an%C(reset)'"
-
 # Generate PR summary in Sublime Text from commit subject and body
 alias supr="
   git log origin..HEAD --reverse --format='- [%s](%H)%n%b' | hx"
