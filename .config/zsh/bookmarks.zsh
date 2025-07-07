c74878ed589bb27d13f9cff535a6acf84bcef88f
Stephan Koenig <stephan.koenig@gmail.com>
2025-07-07 14:01:17 -0700

    Remove bookmarks since most navigation uses fzf

--- .config/zsh/bookmarks.zsh
+++
@@ -1,7 +0,0 @@
-# To create bookmark, `ln -s ~/.bookmarks/@<shortcut> ~/path/to/<shortcut>`
-
-# Bookmarks for shell navigation
-if [ -d "$HOME/.bookmarks" ]; then
-  export CDPATH=".:$HOME/.bookmarks:/"
-  alias goto="cd -P"
-fi
