8a313fc16ca1c4b48144fa50275af3d9b42536da
Stephan Koenig <stephan.koenig@gmail.com>
2026-06-02 09:42:44 -0700

    Update Home Manager

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -13,7 +13,7 @@
   # You should not change this value, even if you update Home Manager. If you do
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
+  home.stateVersion = "26.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -25,14 +25,13 @@
       # gh
       # git
       # helix
+      # jarl
       # lazygit
       # lsd
       # mas
       nix-direnv
       nixd
       nixfmt
-      # quarto
-      # R
       # tldr
       # tree
       # yadm
